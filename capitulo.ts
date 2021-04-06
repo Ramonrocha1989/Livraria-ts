ebda3c74a194b8b93ac4a95b36f00fca62c80a52
Vaniel Timm Koschier <vanielpdc@gmail.com>
2021-04-06 18:07:53 -0300

    creation of the chapter class

---
+++ capitulo.ts
@@ -0,0 +1,21 @@
+export default class Capitulo{
+
+
+    private _titulo:string
+    private _texto:string
+    
+    constructor(titulo:string, texto: string){
+        this._titulo = titulo
+        this._texto = texto
+
+    }
+    public get titulo():string{
+        return this._titulo
+    }
+
+    public get texto():string{
+        return this._texto
+    }
+
+    }
+    
