f051c2657bc531d5f9aac8989dfb4d33c416488c
Ramon Rocha <ramonrocha1989@gmail.com>
2021-04-06 11:56:48 -0300

    update class autor

--- autor.ts
+++ autor.ts
@@ -1,9 +1,19 @@
 export default class Autor {
-    constructor(
-        private _nome: string,
-        private _DateNasc: Date,
+    private _nome: string;
+    private _dateNasc: Date;
 
-    ) { }
+    constructor(nome: string, dateNasc: Date) {
+        this._nome = nome;
+        this._dateNasc = dateNasc;
+    }
+
+    public get nome():string{
+        return this._nome
+    }
+
+    public get dateNasc():Date{
+        return this._dateNasc;
+    }
 
 
 }
